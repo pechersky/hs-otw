0f604a7e2c4736d3e589426b35130430d9cf93df
Yakov Pechersky <ffxen158@gmail.com>
2018-02-03 11:46:56 -0500

    simplify cookie modification lenses

--- src/Natas/Natas5.hs
+++ src/Natas/Natas5.hs
@@ -5,7 +5,7 @@ import qualified Data.ByteString.Lazy.Char8   as C
 import           Data.Maybe                   (catMaybes, listToMaybe)
 import qualified Data.Text                    as T
 import           Data.Text.Encoding           (decodeUtf8)
-import           Network.HTTP.Client.Internal (createCookieJar, expose)
+import           Network.HTTP.Client.Internal (createCookieJar)
 import           Network.Wreq
 import           Text.HTML.TagSoup
 
@@ -13,13 +13,10 @@ import           Natas.Natas
 
 solution :: Solution
 solution = do
-  opts <- loginOptions 5
-  req <- getWith opts (parentUri 5)
-  let Just cki = (listToMaybe . expose . view responseCookieJar) req
-      ckiopts =
-        opts &
-        set cookies (pure (createCookieJar (pure (cki & cookieValue .~ "1"))))
-  ckireq <- getWith ckiopts (parentUri 5)
+  req <- accessLevel 5
+  let ckis = req ^.. responseCookie "loggedin"
+      ckiopt = cookies .~ pure (createCookieJar (ckis <&> cookieValue .~ "1"))
+  ckireq <- accessLevel' 5 (parentUri 5) ckiopt
   let body = (decodeUtf8 . C.toStrict) $ ckireq ^. responseBody
       match =
         (filter ("natas6" `elem`) .
