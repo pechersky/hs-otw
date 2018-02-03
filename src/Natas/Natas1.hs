41c6831ea2328009dab9f2409394d0bb8397de5e
Yakov Pechersky <ffxen158@gmail.com>
2018-02-03 17:44:57 -0500

    factor out workupComments

--- src/Natas/Natas1.hs
+++ src/Natas/Natas1.hs
@@ -1,26 +1,9 @@
 module Natas.Natas1 where
 
-import           Control.Lens
-import qualified Data.ByteString.Lazy.Char8 as C
-import           Data.Maybe                 (catMaybes, listToMaybe)
-import qualified Data.Text                  as T
-import           Data.Text.Encoding         (decodeUtf8)
-import           Network.Wreq
-import           Text.HTML.TagSoup
-
 import           Natas.Natas
 import           Natas.Parse
 
 solution :: Solution
 solution = do
   req <- accessLevel 1
-  pure $ workupRequest req
-
-workupRequest :: CResponse -> Answer
-workupRequest r = last . T.words <$> targetTag
-  where
-    comments =
-      fmap (decodeUtf8 . C.toStrict) .
-      catMaybes . fmap fromTagComment . parseTags $
-      r ^. responseBody
-    targetTag = listToMaybe . filter (("natas2" `elem`) . T.words) $ comments
+  pure $ workupComments ("natas2" `elem`) req
--- src/Natas/Parse.hs
+++ src/Natas/Parse.hs
@@ -1,9 +1,26 @@
 module Natas.Parse where
 
+import           Control.Lens
+import qualified Data.ByteString.Lazy.Char8 as C
+import           Data.Maybe                 (catMaybes, listToMaybe)
+import qualified Data.Text                  as T
+import           Data.Text.Encoding         (decodeUtf8)
+import           Network.Wreq
 import           Text.HTML.TagSoup
+
+import           Natas.Natas
 
 fromTagComment :: Tag a -> Maybe a
 fromTagComment =
   \case
     TagComment str -> Just str
     _ -> Nothing
+
+workupComments :: ([T.Text] -> Bool) -> CResponse -> Answer
+workupComments predicate req = last . T.words <$> targetTag
+  where
+    comments =
+      fmap (decodeUtf8 . C.toStrict) .
+      catMaybes . fmap fromTagComment . parseTags $
+      req ^. responseBody
+    targetTag = listToMaybe . filter (predicate . T.words) $ comments
