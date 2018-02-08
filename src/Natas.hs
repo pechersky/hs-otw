ec80ebd01580af3d7d76ad1377a3c3b8c98b400f
Yakov Pechersky <pechersk@deshawresearch.com>
2018-02-08 17:12:08 -0500

    natas16 soln

--- src/Natas.hs
+++ src/Natas.hs
@@ -28,6 +28,7 @@ import           Natas.Natas12             (solution)
 import           Natas.Natas13             (solution)
 import           Natas.Natas14             (solution)
 import           Natas.Natas15             (solution)
+import           Natas.Natas16             (solution)
 
 challenges :: M.Map Int Solution
 challenges = M.fromList $(getChallenges)
