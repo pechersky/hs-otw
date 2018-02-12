9745f15125c5149bf570e13638aacbab981a1fea
Yakov Pechersky <pechersk@deshawresearch.com>
2018-02-12 11:21:09 -0500

    factor out StatusCode catching

--- src/Natas/Natas.hs
+++ src/Natas/Natas.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Natas.Natas where
 
+import           Control.Exception    (catchJust)
+
+import qualified Data.ByteString      as B
 import           Data.ByteString.Lazy (ByteString, toStrict)
 import           Data.Text            (Text)
 import qualified Data.Text            as T
 import           Data.Text.Encoding   (decodeUtf8, encodeUtf8)
 import qualified Data.Text.IO         as TIO
 
+import           Network.HTTP.Client  (HttpException (HttpExceptionRequest), HttpExceptionContent (StatusCodeException))
 import           Network.Wreq
 import           Network.Wreq.Types   (Postable)
 
@@ -19,8 +25,7 @@ parentUri level = "http://natas" ++ show level ++ ".natas.labs.overthewire.org"
 getLevel :: Int -> IO (Response ByteString)
 getLevel level = getLevel' level (parentUri level) id
 
-getLevel' ::
-     Int -> String -> (Options -> Options) -> IO (Response ByteString)
+getLevel' :: Int -> String -> (Options -> Options) -> IO (Response ByteString)
 getLevel' level uri modifyOptions = do
   opts <- modifyOptions <$> loginOptions level
   getWith opts uri
@@ -53,3 +58,20 @@ writePassword n = TIO.writeFile ("passwords/natas" ++ show n) . T.strip
 
 reqBody :: Response ByteString -> Text
 reqBody = decodeUtf8 . toStrict . view responseBody
+
+isStatusCodeExceptionHandler ::
+     (forall body. Response body -> B.ByteString -> a)
+  -> HttpException
+  -> Maybe a
+isStatusCodeExceptionHandler converter (HttpExceptionRequest _req (StatusCodeException resp respbody)) =
+  Just (converter resp respbody)
+isStatusCodeExceptionHandler _converter _exception = Nothing
+
+catchResponseHandler ::
+     (forall body. Response body -> a) -> IO a -> IO a
+catchResponseHandler converter req =
+  catchJust (isStatusCodeExceptionHandler (const . converter)) req pure
+
+catchResponseBodyHandler :: (B.ByteString -> a) -> IO a -> IO a
+catchResponseBodyHandler converter req =
+  catchJust (isStatusCodeExceptionHandler (const converter)) req pure
--- src/Natas/Natas22.hs
+++ src/Natas/Natas22.hs
@@ -1,14 +1,10 @@
 module Natas.Natas22 where
 
-import           Control.Exception   (catchJust)
+import           Data.Text          (Text)
+import           Data.Text.Encoding (decodeUtf8)
+import           Safe               (headMay, lastMay)
 
-import           Data.Text           (Text)
-import           Data.Text.Encoding  (decodeUtf8)
-import           Safe                (headMay, lastMay)
-
-import           Network.HTTP.Client (HttpException (HttpExceptionRequest),
-                                      HttpExceptionContent (StatusCodeException))
-import           Network.Wreq        (redirects)
+import           Network.Wreq       (redirects)
 
 import           Control.Lens
 
@@ -18,13 +14,8 @@ import           Natas.Parse
 solution :: Solution
 solution = do
   let req = getLevel' 22 (parentUri 22 ++ "/?revelio") (redirects .~ 0)
-  caughtBody <- catchJust isStatusCodeExceptionBody (reqBody <$> req) pure
+  caughtBody <- catchResponseBodyHandler decodeUtf8 (reqBody <$> req)
   attemptParse caughtBody
-
-isStatusCodeExceptionBody :: HttpException -> Maybe Text
-isStatusCodeExceptionBody (HttpExceptionRequest _req (StatusCodeException _resp bs)) =
-  Just (decodeUtf8 bs)
-isStatusCodeExceptionBody _exception = Nothing
 
 attemptParse :: Text -> IO (Maybe Text)
 attemptParse body =
